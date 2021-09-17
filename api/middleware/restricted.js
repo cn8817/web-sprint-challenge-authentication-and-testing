05e8a7ee15138067ca95e2a9331698d38d8f8be7
cn8817 <christinenguyen1433@gmail.com>
2021-09-17 16:51:52 -0700

    fixing codegrade

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -26,13 +26,4 @@ jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
  })
  }
 
-  /*
-    IMPLEMENT
-    1- On valid token in the Authorization header, call next.
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
-
      
